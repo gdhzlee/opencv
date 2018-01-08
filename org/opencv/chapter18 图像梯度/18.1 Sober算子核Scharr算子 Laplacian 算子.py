f72a7cce9f7cb8153158f5d0d1ce0c4d3589802c
gdhzlee <gdhzlee@163.com>
2018-01-08 21:07:51 +0800

    17

--- org/opencv/chapter18 图像梯度/18.1 Sober算子核Scharr算子 Laplacian 算子.py
+++ org/opencv/chapter18 图像梯度/18.1 Sober算子核Scharr算子 Laplacian 算子.py
@@ -12,16 +12,42 @@ Laplacian 算子
 拉普拉斯算子可以使用二阶导数的形式定义，可假设其离散实现类似于二
 阶Sobel 导数，事实上，OpenCV 在计算拉普拉斯算子时直接调用Sobel 算
 子。拉普拉斯滤波器使用的卷积核：
-kernel = [ 0  1 0 ]
-         [ 1 -4 1 ]
-         [ 1  1 0 ]
+kernel = [ 0  1  0 ]
+         [ 1 -4  1 ]
+         [ 1  1  0 ]
 """
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt;
 
 img = cv2.imread("C:\\Users\\lee\\Desktop\\1.jpg",0);
+"""
+cv2.Laplacian(src, ddepth[, dst[, ksize[, scale[, delta[, borderType]]]]]) → dst
+Calculates the Laplacian of an image.
+    src – Source image.
+    ddepth – Desired depth of the destination image.
+    ksize – Aperture size used to compute the second-derivative filters. See getDerivKernels() for details. The size must be positive and odd.
+    scale – Optional scale factor for the computed Laplacian values. By default, no scaling is applied. See getDerivKernels() for details.
+    delta – Optional delta value that is added to the results prior to storing them in dst .
+    borderType – Pixel extrapolation method. See borderInterpolate for details.
+"""
 laplacian = cv2.Laplacian(img,cv2.CV_64F);
+
+"""
+cv2.Sobel(src, ddepth, dx, dy[, dst[, ksize[, scale[, delta[, borderType]]]]]) → dst
+Calculates the first, second, third, or mixed image derivatives using an extended Sobel operator.
+    src – input image.
+    dst – output image of the same size and the same number of channels as src .
+    ddepth –output image depth; the following combinations of src.depth() and ddepth are supported:
+        src.depth() = CV_8U, ddepth = -1/CV_16S/CV_32F/CV_64F
+        src.depth() = CV_16U/CV_16S, ddepth = -1/CV_32F/CV_64F
+        src.depth() = CV_32F, ddepth = -1/CV_32F/CV_64F
+        src.depth() = CV_64F, ddepth = -1/CV_64F
+        when ddepth=-1, the destination image will have the same depth as the source; in the case of 8-bit input images it will result in truncated derivatives 截断面.
+    xorder – order of the derivative x.
+    yorder – order of the derivative y.
+    ksize – size of the extended Sobel kernel; it must be 1, 3, 5, or 7.
+"""
 #参数为 1,0 只在x方法求一阶导数，最大可以求二阶导数
 sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5);
 #参数为 0,1 只在y方法求一阶导数，最大可以求二阶导数
